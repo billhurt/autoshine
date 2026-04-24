ab2fcfdda9b44ae167b0b25e9f90e73b15f30e73
Bill Hurt <william.hurt6@gmail.com>
2026-04-24 10:18:49 +0100

    remove recording_status

--- twilio_webhooks/twiml.py
+++ twilio_webhooks/twiml.py
@@ -3,7 +3,7 @@ from twilio.twiml.voice_response import VoiceResponse
 def missed_call_response():
     response = VoiceResponse()
     response.say(
-        "Hi, sorry we missed your call. Please reply to the text message we’ve just sent, or feel free to leave a voicemail after the tone. "
+        "Hi, sorry we missed your call. Please reply to the text message we've just sent, or feel free to leave a voicemail after the tone. "
         "Thanks for calling Outcast detailing.",
         voice='Polly.Amy'
     )
@@ -11,8 +11,5 @@ def missed_call_response():
         max_length=60,
         action='/webhooks/twilio/voicemail/',
         method='POST',
-        recording_status_callback='https://web-production-79971.up.railway.app/webhooks/twilio/recording-status/',
-        recording_status_callback_method='POST',
-        recording_status_callback_event='in-progress',
     )
     return str(response)
--- twilio_webhooks/urls.py
+++ twilio_webhooks/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('missed-call/', views.missed_call, name='missed_call'),
-    path('recording-status/', views.recording_status, name='recording_status'),
     path('voicemail/', views.voicemail, name='voicemail'),
     path('incoming-sms/', views.incoming_sms, name='incoming_sms'),
     path('voicemail/play/<str:recording_sid>/', views.play_voicemail, name='play_voicemail'),
